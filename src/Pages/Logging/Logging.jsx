a4c9555f8fe7da181e8728a66bfe9fcb4019477c
Nikolay Litvinenko <Litvinenko_n.v@mail.ru>
2024-01-07 18:58:57 +0300

    commit

--- src/Pages/Logging/Logging.jsx
+++ src/Pages/Logging/Logging.jsx
@@ -3,9 +3,27 @@ import Container from "../../components/UI/Container/Container";
 
 function Logging({ children }) {
   return (
-    <Container>
-      <div className={styles.wrapper}></div>;
-    </Container>
+    <div className={styles.wrapper}>
+      <Container>
+        <div className={styles["container-heading"]}>
+          <h2 className={styles.heading}>Войти</h2>
+        </div>
+
+        <form className={styles["container-form"]}>
+          <h2 className={styles["error-text"]}>Неверный логин или пароль</h2>
+
+          <div className={styles["container-input"]}>
+            <label className={styles.label}>Email address</label>
+            <input className={styles.input} type="text" />
+          </div>
+
+          <div className={styles["container-input"]}>
+            <label className={styles.label}>Password</label>
+            <input className={styles.input} type="password" />
+          </div>
+        </form>
+      </Container>
+    </div>
   );
 }
 
--- src/components/UI/Container/Container.jsx
+++ src/components/UI/Container/Container.jsx
@@ -2,8 +2,7 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   background: var(--background-card);
-  width: 300px;
-  height: 200px;
+  border-radius: 5px;
 `;
 
 function Container({ children }) {
